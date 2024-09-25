d1806573e241e19aab35685b2c013d3be64fd5d3
Marina Elizarova <seagirl1110@gmail.com>
2024-09-25 12:02:00 +0300

    feat: add redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  products: [],
+  cart: [],
+};
+
+const productReducer = (state = initialState, { type, payload }) => {
+  return state;
+};
+
+export default productReducer;
